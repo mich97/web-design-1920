3ceb3d20b3b6f8eb1a4b767367440bfae0e938f0
Michel <michel_abdad@hotmail.com>
2020-04-15 15:53:48 +0200

    ctrl navigating

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('keydown', function (event) {
-    if (event.keyCode === 13 && event.target.nodeName === 'SELECT') {
+    if (event.keyCode === 17 && event.target.nodeName === 'SELECT') {
         const form = event.target.form;
         const index = Array.prototype.indexOf.call(form, event.target);
         form.elements[index + 1].focus();
