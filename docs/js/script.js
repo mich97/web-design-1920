f59a9258f4d06c64db8101f0ac4a897071393964
Michel <michel_abdad@hotmail.com>
2020-04-15 15:50:06 +0200

    add default datepicker

---
+++ docs/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 13 && event.target.nodeName === 'SELECT') {
+        const form = event.target.form;
+        const index = Array.prototype.indexOf.call(form, event.target);
+        form.elements[index + 1].focus();
+        event.preventDefault();
+    }
+});
