497d6f5f4dc2b233d604ccbcc9f3672a64666664
Michel <michel_abdad@hotmail.com>
2020-04-22 16:29:32 +0200

    mostly styling

--- docs/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('keydown', function (event) {
-    if (event.keyCode === 17 && event.target.nodeName === 'SELECT') {
-        const form = event.target.form;
-        const index = Array.prototype.indexOf.call(form, event.target);
-        form.elements[index + 1].focus();
-        event.preventDefault();
-    }
-});
